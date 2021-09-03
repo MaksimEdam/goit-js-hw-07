0f25858568c8717b2dde8a75c56c329307a3b995
MaksimEdam <Edamenko6@gmail.com>
2021-09-03 21:13:40 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,7 @@
-const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
-];
+const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы'];
+const ulEl = document.querySelector('#ingredients');
+
+const list = ingredients.reduce((arr, ingred) => arr + `<li>${ingred}</li>`, '');
+
+ulEl.innerHTML = list;
+console.log(ulEl);
